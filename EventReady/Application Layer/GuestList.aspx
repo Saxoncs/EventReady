023b10e2e21059edc8f365e1b6619806042bfc35
Robert Piercy <c3282915@uon.edu.au>
2019-08-14 19:51:35 +1000

    Email Test (Complete)

--- EventReady/Application Layer/GuestList.aspx
+++ EventReady/Application Layer/GuestList.aspx
@@ -2,4 +2,8 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+
+    <asp:Button runat="server" OnClick="btnInviteGuest" Text="Send Email"/>
 </asp:Content>
--- EventReady/Application Layer/GuestList.aspx.cs
+++ EventReady/Application Layer/GuestList.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
 
 namespace EventReady.Application_Layer
 {
@@ -12,6 +13,63 @@ namespace EventReady.Application_Layer
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        private string invitationMessage(bool isHtml)
+        {
+            //Content if html is available 
+            if (isHtml)
+                return "<html><head><title>Guest Invitation"
+                        + " </title></head>"
+                        + "<body><img src=cid:logoImage>" + "<br />"
+                        + "<h3> This is a test email</h3>"
+                        + "<p>Test</p></body></html>";
+            //PLain text if html is not
+            else
+                return "Guest Invitation \r\n"
+                        + "Test";
+        }
+
+        protected void btnInviteGuest(object sender, EventArgs e)
+        {
+            SmtpClient client = new SmtpClient();
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            System.Net.NetworkCredential credentials =
+            new System.Net.NetworkCredential("eventready281@gmail.com", "eventready123");
+            client.UseDefaultCredentials = false;
+            client.Credentials = credentials;
+            //------------------------------------------------------------------------------
+
+
+            //Add email content including from, to, subject and body
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("eventready281@gmail.com");
+            msg.To.Add(new MailAddress("robertpiercy2199@gmail.com"));
+            msg.Subject = "EventReady - Invitation";
+            //If html does not exist return non-html email
+            msg.Body = invitationMessage(false);
+
+            //create an alternate HTML view that includes images and formatting 
+            string html = invitationMessage(true);
+            AlternateView view = AlternateView
+                .CreateAlternateViewFromString(
+                    html, null, "text/html");
+
+            //add the HTML view to the message and send
+            msg.AlternateViews.Add(view);
+
+            try
+            {
+                client.Send(msg);
+                //Send to main page with pop message about sent email
+            }
+            catch
+            {
+                //Display error message for email failing to send 
+
+            }
         }
     }
 }
