f95d5080bf006b77658aae035441466dd6b67903
Saxoncs <saxon.cs@outlook.com>
2019-08-27 14:06:37 +1000

    Slight fix to email DAL
    removed a useless function

--- EventReady/Data Access Layer/EmailDataAccess.cs
+++ EventReady/Data Access Layer/EmailDataAccess.cs
@@ -90,13 +90,6 @@ namespace EventReady.Data_Access_Layer
         }
 
         //function takes a guest class from the business layer and adds it to the database - Saxon
-        [DataObjectMethod(DataObjectMethodType.Insert)]
-
-        public IEnumerable AddGuest(string userId, string name, string something) //DELETE THIS LATER
-        {
-
-        }
-
         public static int AddGuest(string userId, Guest guest)
 
         {
