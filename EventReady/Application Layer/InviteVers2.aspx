09b8ff03ae588c7ee488a3578497ac111407268c
Robert Piercy <c3282915@uon.edu.au>
2019-10-25 13:26:39 +1100

    Invite Guests
    Allows user to add text boxes and then can enter emails for each text box so that everyone entered can receive an email.

---
+++ EventReady/Application Layer/InviteVers2.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Application Layer/MasterPage.Master" AutoEventWireup="true" CodeBehind="InviteVers2.aspx.cs" Inherits="EventReady.Application_Layer.InviteVers2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+    <label>Invite Guests to your invite using an email</label>
+
+    <asp:TextBox runat="server" ID="CounterTextBox" AutoPostBack="true" />
+    <asp:button runat="server" Text="Add Field" ID="btnAddField" OnClick="btnAddField_Click"/> 
+    <asp:button runat="server" Text="Confirm Guest List" ID="btnConfirmGuests" OnClick="btnConfirmGuests_Click"/> 
+    <asp:PlaceHolder ID="PlaceHolder1" runat="server">
+    </asp:PlaceHolder>
+    
+</asp:Content>
---
+++ EventReady/Application Layer/InviteVers2.aspx.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.IO;
+
+namespace EventReady.Application_Layer
+{
+    public partial class InviteVers2 : System.Web.UI.Page
+    {
+        TextBox tb;
+        static int i = 0;
+        static int p = 0;
+        static int j = 0;
+        int count = 2;
+        int z = 0;
+        //Date: 23/mar/14
+        //Author: Win
+        //URL:
+        //USE: to get values out of dynamic created textboxes
+
+        protected
+            private List<string> TextBoxIdCollection
+        {
+            get
+            {
+                var collection = ViewState["TextBoxIdCollection"] as List<string>;
+                return collection ?? new List<string>();
+            }
+            set { ViewState["TextBoxIdCollection"] = value; }
+        }
+        void Page_Load(object sender, EventArgs e)
+        {
+
+            foreach (string textboxId in TextBoxIdCollection)
+            {
+                var textbox = new TextBox { ID = textboxId };
+                PlaceHolder1.Controls.Add(textbox);
+            }
+            
+        }
+
+        
+        protected void btnAddField_Click(object sender, EventArgs e)
+        {
+            //Author: Pranay Rana
+            //Date: 21/11/19
+            //https://stackoverflow.com/questions/13488006/dynamically-add-a-new-text-box-when-clicking-a-button
+            //Use to add a texbox field on button click
+            var collection = new List<string>();
+            int total;
+            
+            if (Int32.TryParse(CounterTextBox.Text, out total))
+            {
+                for (int i = 1; i <= total; i++)
+                {
+                    var textbox = new TextBox { ID = "TextBox" + i };
+                    textbox.Text = textbox.ID;
+                    collection.Add(textbox.ID);
+                    PlaceHolder1.Controls.Add(textbox);
+                    
+                }
+                TextBoxIdCollection = collection;
+            }
+            
+             
+            /*i++;
+          
+            for (j = 1; j <= i; j++)
+            {
+                
+                tb = new TextBox();
+                tb.Text = "TextBox" + j.ToString();
+                tb.ID = "TextBox" + j.ToString();
+                PlaceHolder1.Controls.Add(tb);
+                //PlaceHolder1.Controls.Add(new TextBox());
+                collection.Add(tb.ID); 
+
+               
+
+            }
+            TextBoxIdCollection = collection; */
+
+
+            /*int index = pnlTextBoxes.Controls.OfType<TextBox>().ToList().Count + 1;
+            TextBox txt = new TextBox();
+            txt.ID = "TextBox" + index;
+            pnlTextBoxes.Controls.Add(txt);
+            Literal lt = new Literal();
+            lt.Text = "<br />";
+            pnlTextBoxes.Controls.Add(lt);*/
+            //this.CreateTextBox("TextBox" + index);
+
+            //IterateThroughChildren(this);
+            /*pnlTextBoxes.Controls.Clear();
+            int index = pnlTextBoxes.Controls.OfType<TextBox>().ToList().Count + 1;
+            this.CreateTextBox("txtDynamic" + index);*/
+
+            // z = Convert.ToInt32(HowManyToGenerateTextBox.Text);
+            //Panel1.Controls.Clear();
+
+
+
+            /* for (int i = 0; i < z; i++)
+             {
+                 TextBox s = new TextBox();
+                 s.ID = "tb" + i.ToString();
+                 Session[s.ID] = s;
+                 Panel1.Controls.Add(s);
+             }*/
+
+        }
+
+
+       /* private void CreateTextBox(string id)
+        {
+
+            TextBox txt = new TextBox();
+            txt.ID = id;
+            pnlTextBoxes.Controls.Add(txt);
+
+            Literal lt = new Literal();
+            lt.Text = "<br />";
+            pnlTextBoxes.Controls.Add(lt);
+
+        }*/
+        /*void IterateThroughChildren(Control parent)
+        {
+            
+            foreach (Control c in parent.Controls)
+            {
+                if (c.GetType().ToString().Equals("System.Web.UI.WebControls.TextBox")
+                      && c.ID == null)
+                {
+                    ((TextBox)c).Text = "TextBox" + count.ToString();
+                    ((TextBox)c).ID = "TextBox"+count.ToString();
+                    count++;
+                }
+
+                if (c.Controls.Count > 0)
+                {
+                    IterateThroughChildren(c);
+                }
+            }
+        }*/
+
+
+        private string PopulateBody()
+        {
+            string body = string.Empty;
+
+
+            using (StreamReader reader = new StreamReader(Server.MapPath("../Application Layer/EmailTemplate.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            //Adding an image to the email
+
+            //view.LinkedResources.Add(img);
+
+            //body = body.Replace("{ribbonImgPlaceHolder}", img.ContentId);
+
+            return body;
+        }
+        private void SendHtmlFormattedEmail(string body)
+        {
+            //for (p = 1; p <= j; p++)
+            //{ 
+            foreach (Control ctr in PlaceHolder1.Controls)
+            {
+                if (ctr is TextBox)
+                {
+
+
+
+
+
+
+                    //string check = "TextBox" + p.ToString();
+                    //TextBox t = (TextBox)Page.FindControl("TextBox" + p.ToString());
+                    //TextBox tt = this.FindControl("ContentPlaceHolder1_TextBox" + p.ToString()) as TextBox;*/
+
+                    SmtpClient client = new SmtpClient();
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.EnableSsl = true;
+                    client.Host = "smtp.gmail.com";
+                    client.Port = 587;
+                    System.Net.NetworkCredential credentials =
+                    new System.Net.NetworkCredential("eventready281@gmail.com", "eventready123");
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = credentials;
+                    //------------------------------------------------------------------------------
+
+
+                    //Add email content including from, to, subject and body
+                    MailMessage msg = new MailMessage();
+                    msg.From = new MailAddress("eventready281@gmail.com");
+                    msg.To.Add(new MailAddress(((TextBox)ctr).Text));
+                    msg.Subject = "EventReady - Invitation";
+                    //If html does not exist return non-html email
+                    msg.Body = body;
+                    msg.IsBodyHtml = true;
+
+                    AlternateView view = AlternateView
+                     .CreateAlternateViewFromString(
+                         body, null, "text/html");
+                    string imgPathOne = Server.MapPath("../Image/ribbon.png");
+                    LinkedResource img = new LinkedResource(imgPathOne);
+                    img.ContentId = "ribbonImage";
+                    view.LinkedResources.Add(img);
+
+                    msg.AlternateViews.Add(view);
+
+                    try
+                    {
+                        client.Send(msg);
+                        //Send to main page with pop message about sent email
+                    }
+                    catch
+                    {
+                        //Display error message for email failing to send 
+
+                    }
+                }
+                }
+
+            }
+        
+        protected void btnConfirmGuests_Click(object sender, EventArgs e)
+        {
+           
+            string body = this.PopulateBody();
+            this.SendHtmlFormattedEmail(body);
+        }
+    }
+}
