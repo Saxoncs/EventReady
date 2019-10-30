1fb357e44c29abbf31a38eaf8ee7dce1f2ba482d
Saxoncs <saxon.cs@outlook.com>
2019-10-30 20:21:41 +1100

    Removed redundant BL page
    Removed the RSVP BL page and created some groundwork for connecting the RSVP page to the database

--- EventReady/Application Layer/RSVPPage.aspx.cs
+++ EventReady/Application Layer/RSVPPage.aspx.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using EventReady.Business_Layer;
+using static EventReady.Business_Layer.EmailBL;
+using static EventReady.Data_Access_Layer.EmailDataAccess;
 
 namespace EventReady.Application_Layer
 {
     public partial class RSVPPage : System.Web.UI.Page
     {
-        protected string guestList;
+
+        // EmailBL email = new EmailBL();
+
+         protected string guestList;
         protected void Page_Load(object sender, EventArgs e)
         {
-            guestList = Request.QueryString["guestList"];
+             guestList = Request.QueryString["guestList"];
+
+            //0000001 needs to be replaced with the event number of whichever event is clicked
+            //List<Guest> guestList = email.GetActiveGuests("0000001");
         }
     }
 }
